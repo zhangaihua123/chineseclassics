bf33ba4d98bc866c1c4aeb55882a5ce11b74ebf7
zhangaihua123 <3521885957@qq.com>
2020-02-14 12:27:38 +0800

    国学2020

---
+++ src/test/java/com/example/chineseclassics/ChineseclassicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chineseclassics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChineseclassicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
